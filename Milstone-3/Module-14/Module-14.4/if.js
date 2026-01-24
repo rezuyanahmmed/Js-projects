446db783f1e855d7896862048fdee34335260daa
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-01-24 16:52:12 +0600

    added

--- Milstone-3/Module-14/Module-14.4/if.js
+++ Milstone-3/Module-14/Module-14.4/if.js
@@ -1,4 +1,27 @@
+// ------------------------------------
+
 var biriyaniPrice = 200
 if (biriyaniPrice < 300) {
   console.log('give me one packet biriany');
+}
+
+
+// -------------------------------------
+
+var weight = 15;
+if(weight < 20){
+  console.log("I will carry it");
+}
+else{
+  console.log("I will hire a Rickshaw");
+}
+
+// -------------------------------------
+
+var weight = 30;
+if(weight < 20){
+  console.log("I will carry it");
+}
+else{
+  console.log("I will hire a Rickshaw");
 }
