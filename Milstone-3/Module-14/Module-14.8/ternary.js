c64511912760d139c70c322f73b4980395e722e3
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-01-25 02:03:00 +0600

    added

---
+++ Milstone-3/Module-14/Module-14.8/ternary.js
@@ -0,0 +1,30 @@
+
+
+
+// const age = 19;
+// if(age >= 18){
+//   console.log("you can vote");
+// }
+// else{
+//   console.log("You can not vote");
+// }
+
+// ------------Simple ternary-------------------
+
+// age >= 18 ? console.log("you can vote") : console.log("you can not vote");
+
+
+let price = 500;
+const isLeader = true;
+// if(isLeader === true){
+//   price = 0;
+// }
+// else{
+//   price = price + 100
+// }
+// console.log(price);
+
+price = isLeader === true ? 0 : price + 100;
+
+
+
