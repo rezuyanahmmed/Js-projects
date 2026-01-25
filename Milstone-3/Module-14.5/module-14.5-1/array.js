1166201c95522cfd8a9ff86b6b40b538281275b9
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-01-25 16:01:18 +0600

    added

---
+++ Milstone-3/Module-14.5/module-14.5-1/array.js
@@ -0,0 +1,9 @@
+// ARRAY diye amra ekadhik number ekta variable ei rakhte pari. jemon-------
+
+const numbers = [10, 20, 30, 40, 50]
+
+const friends = ['Shahed', 'Rakibul', 'Ashik','Rabbi']
+
+const passed = [true, false, true, false, true]
+
+const mixedArray = [10, 'rafi', true]
