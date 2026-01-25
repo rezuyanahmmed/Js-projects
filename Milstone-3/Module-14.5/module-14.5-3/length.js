529a4fa6b658cc86590372e258726fbf787b818f
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-01-25 19:34:03 +0600

    add

---
+++ Milstone-3/Module-14.5/module-14.5-3/length.js
@@ -0,0 +1,18 @@
+// const numbers = [20, 21, 23, 24, 5, 20, 21, 23, 24, 5, 23, 65, 85, 69,]
+// console.log(numbers.length);
+
+ 
+// -----------------------INDEX NUMBER----------------------------------
+
+const numbers = [20, 21, 23, 24, 5, "r", 21, 23, 24, 5, "rafi", 65, 85, 69,]
+
+console.log(numbers[5]);
+
+const fourth = numbers [3]  // ei system e alada kore ber kora jay
+console.log(fourth);
+
+
+// -----------------set or update element-------------------------------
+
+numbers[5] = 50;  //[ekahne main main namne e 5 number index ta 50 hoye jabe]
+console.log(numbers);
