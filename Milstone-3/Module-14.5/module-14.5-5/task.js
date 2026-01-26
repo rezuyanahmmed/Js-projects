13ca9da38ae16426dc6bccc1421877653378c1e8
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-01-26 20:15:58 +0600

    added

---
+++ Milstone-3/Module-14.5/module-14.5-5/task.js
@@ -0,0 +1,11 @@
+// Declare an array with 5 fruits
+const fruits = ["apple", "banana", "orange", "mango", "grape"];
+
+// Console log the 3rd index element (index 3)
+console.log(fruits[3]); // mango
+
+// Change the value of the 2nd index element (index 2)
+fruits[2] = "jambura";
+
+// Console log the final array
+console.log(fruits);
