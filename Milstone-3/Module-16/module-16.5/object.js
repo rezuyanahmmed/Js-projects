29236ad8a616e58505322d2c0e0243cdb764b2ef
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-01-29 02:17:35 +0600

    added

---
+++ Milstone-3/Module-16/module-16.5/object.js
@@ -0,0 +1,11 @@
+const subjects = {
+  name: 'web-design',
+  teacher: 'Rafi sir',
+  examDate: '30 feb',
+  chapters: ['first','second','third'],
+  exams{
+    first: 30 feb,
+    second: 2 march,
+    third: 5 march,
+  }
+}
