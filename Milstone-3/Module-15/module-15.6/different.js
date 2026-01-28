ef1e53c541f6b68192ecac65086232472dc4b1a9
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-01-28 15:33:14 +0600

    added

---
+++ Milstone-3/Module-15/module-15.6/different.js
@@ -0,0 +1,38 @@
+//different ways to use for
+
+// for (let i = 1; i <= 30; i++) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+
+//   }
+// }
+
+//-----------------------------------------------------------------
+
+// for (let i = 1; i <= 30; i++) {
+//   if (i % 5 === 0 || i % 3 === 0) {
+//     console.log(i);
+
+//   }
+// }
+
+//----------------------------------------------------
+
+// for (let i = 1; i <= 30; i++) {
+//   if (i % 5 === 0 && i % 3 === 0) {
+//     console.log(i);
+//   }
+// }
+
+//give me the sum of numbers from 1 to 20 that are divisible by 3
+
+let total = 0;
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0) {
+    console.log(i);
+    total = total + i;
+    console.log('total:', total);
+
+  }
+}
+console.log('total of all:', total);
