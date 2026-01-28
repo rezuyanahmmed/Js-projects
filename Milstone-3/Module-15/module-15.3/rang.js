7a04d102ea97d39059cc43d815be04f0803bc176
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-01-28 10:29:28 +0600

    added

---
+++ Milstone-3/Module-15/module-15.3/rang.js
@@ -0,0 +1,26 @@
+//----------------normal sum---------------------------------
+
+// let num = 1;
+// let sum = 0;
+// while(num <= 10){
+//   console.log(num);
+//   sum = sum + num
+//   console.log('sum:', sum);
+  
+//   num++;  //eta na dile loop ajibon e colte thakbe thambe na,tai eta khub e joruri
+  
+// }
+
+
+//-----------------------even number---------------------------
+
+let num = 1;
+while(num < 10){
+  console.log(num);
+  num++;
+  if(num % 2 === 0) {   //ekahen bishoy ta holo === 0 jodi dei tahole EVEN ta ver hobe karon jor songkha vag korle vagsesh 0 e hoy....... r jodi 1 dei tahole sob OOD number ber hobe
+
+    console.log('even number:', num);
+  
+  }
+}
