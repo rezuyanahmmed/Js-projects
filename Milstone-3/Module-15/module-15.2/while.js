3f8ca06a4076e798295b98976dd0af509b9f90a3
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-01-28 09:16:15 +0600

    added

---
+++ Milstone-3/Module-15/module-15.2/while.js
@@ -0,0 +1,11 @@
+//-----------while loop-------------------
+
+// while loop calale bar bar coltei thakbe
+
+let number = 0;
+while(number < 5){
+  console.log('loooooping',number);
+  // number = number + 1;
+  // number += 1;
+  // number++;
+}
