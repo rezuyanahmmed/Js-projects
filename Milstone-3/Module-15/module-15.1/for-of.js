a47422dedf44eb4797c9b3b6fd2675f4247fd4ed
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-01-28 08:56:11 +0600

    added

---
+++ Milstone-3/Module-15/module-15.1/for-of.js
@@ -0,0 +1,11 @@
+//---------------------normal lop----------------------------------
+
+const numbers = [10, 20, 30, 40, 50,]
+console.log(numbers);
+
+//------------------for diye normal loop-----------------------------
+
+for(const num of numbers){
+  console.log(num);
+  
+}
