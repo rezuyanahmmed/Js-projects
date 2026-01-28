a47659f408bdfc31823d52488213c01e02648df5
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-01-28 11:28:14 +0600

    added

---
+++ Milstone-3/Module-15/module-15.4/for-loop.js
@@ -0,0 +1,14 @@
+// let num = 0; //loop variable
+// while(num < 5){  //loop condition
+//    console.log(num);  //loop statment
+//    num++;  //loop variablr change 
+// }
+
+// for(loop variable declaration; loop condition)
+ 
+ 
+
+for(let num = 0; num < 5; num++){
+  console.log(num);
+  
+}
