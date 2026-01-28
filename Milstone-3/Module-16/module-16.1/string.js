a52b9aa456c3695a78afd5c252c4c14f297d3ec9
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-01-28 22:42:18 +0600

    added

---
+++ Milstone-3/Module-16/module-16.1/string.js
@@ -0,0 +1,10 @@
+// String is immutable------not changeable
+//array is mutable------you can change the array elements
+
+const number = [10, 20, 30, 40, 50,]
+
+number[1] = 10  //etar karone index 1 er man change hoye geche
+console.log(number); 
+
+
+
