38007bc40230989c18807570e36cabb305baef80
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-01-31 19:39:20 +0600

    added

---
+++ Milstone-3/Module-16.5/module-16.5-3/sort.js
@@ -0,0 +1,16 @@
+/*
+ * Sort ----->
+ * Ascending------> smaller to larger
+ * descending-----> larger to smaller
+ */
+
+const numbers = [4, 5, 9, 1, 2, 3, 7];
+const numbers_asc = numbers.sort(); //normally 1st to last sajabe....
+console.log(numbers_asc);
+
+//----------name diy sort-------------------
+
+const people = ['refi','Safi','kafi','anis']
+const ple_asc = people.sort(); // ekhane sajabe jeta letter er age ache seta age,ekhon jodi ekhane kono ekta name er 1st letter ta capital kore dei tahole oita sobar age ashbe....
+console.log(ple_asc);
+
