4b4271f4325058af3303cca3a9d5f1437f039a33
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-01 16:39:35 +0600

    added

---
+++ Milstone-3/Module-17/module-17.5/return.js
@@ -0,0 +1,8 @@
+function cutHalf(number) {
+  const half = number / 2;
+  return half;
+}
+// cutHalf(10);
+const output = cutHalf(10);  //ekhane cutHalf tar mane holo amnra function ta k call kortechi...............
+// return er khetre emon ekta variable diye declare kore consol.log er vitor oi variable er name ta dilei output cole ashbe
+console.log(output);
