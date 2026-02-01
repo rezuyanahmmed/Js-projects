78ffb033d4220a3c0cb9a20c0178283f232c54ed
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-01 16:00:30 +0600

    added

---
+++ Milstone-3/Module-17/module-17.2/perameter.js
@@ -0,0 +1,16 @@
+function square(number) {
+  const borgo = number * number;
+  console.log('equal=', borgo);
+
+}
+square(10); // ekhane square ta holo variable er name
+square(20);
+
+//------------------------------------------
+
+function add(num1, num2, num3) {
+  const sum = num1 + num2 + num3;
+  console.log('equal=', sum)
+
+}
+add(20, 20, 10)
