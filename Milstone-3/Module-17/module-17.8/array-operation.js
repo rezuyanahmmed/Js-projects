e1540aed6c22114096f5f38291d8c32437d31f0a
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-01 20:32:57 +0600

    added

---
+++ Milstone-3/Module-17/module-17.8/array-operation.js
@@ -0,0 +1,11 @@
+/**
+ * Objects: write a function to give me the sum of all numbers in array
+ * step-1: declare a function
+ * step-2:call cheak weather the function is called properly
+ * step-3:set perameter(s)
+ * step-4: pas the perameter(s), cheak wether perameter is passed in a proper format
+ * step-5:do the function tasks
+ */
+
+
+
