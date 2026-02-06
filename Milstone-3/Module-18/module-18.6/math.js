e69af7b88b70e89178e6a53ceb5bd284383ca7a6
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-06 19:40:49 +0600

    added

---
+++ Milstone-3/Module-18/module-18.6/math.js
@@ -0,0 +1,29 @@
+// Math, abs, round, ceil, floor, and random number
+
+const min = Math.min(21, 34, 95, 85, 32, 14)
+console.log(min);
+//------------------------------------------
+const beshi = Math.max(21, 34, 95, 85, 32, 14);
+console.log(beshi);
+
+console.log(Math.PI); // pai er man ta ber kore dibe...
+console.log(Math.abs(5-10)); //abs ta difference ber kore dibe...
+console.log(Math.round(5.25)); //round er kaj holo jekono ekta doshomik songkha take tar kachakachi purnosongkha kore dibe...
+
+console.log(Math.floor(6.95)); //floor er kaj holo .er porer ongso take bad diye dibe,ekhane 6 dibe...
+console.log(Math.ceil(2.001)); //etar kaj holo doshomik er porer purnosongkha take dibe,mane floor er biporit...
+
+console.log(Math.random()*10); //jekono ekta random doshomil ola songkha ber kore dibe...
+
+const rand = Math.round(Math.random()*10)
+console.log(rand); //math.round dewar karone jekono ekta purnosongkha diteche...
+
+
+
+
+
+
+
+
+
+
