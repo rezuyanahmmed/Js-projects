bd57f087169ab4df13732c1e873d800909019e84
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-07 12:56:23 +0600

    added

---
+++ Milstone-3/Module-19/module-19.1/max.js
@@ -0,0 +1,11 @@
+const salman = 80;
+const kamal = 60;
+
+if(salman > kamal){
+  console.log('salman will get');
+  
+}
+else{
+  console.log('kamal will get');
+  
+}
---
+++ Milstone-3/Module-19/module-19.1/max3.js
@@ -0,0 +1,21 @@
+const rafi = 56;
+const kafi = 85;
+const shafi = 175;
+
+if (rafi > kafi && rafi > shafi) {
+  console.log("rafi is the boss");
+
+}
+else if (kafi > rafi && kafi > shafi) {
+  console.log("kafi is the boss");
+
+}
+else {
+  console.log('shafi is the boss');
+
+}
+
+//---------unlimited number theke max  ta ber korar niyom---------
+
+const boro = Math.max(12, 1, 4, 25, 6, 7, 68, 74, 6)
+console.log(boro);
