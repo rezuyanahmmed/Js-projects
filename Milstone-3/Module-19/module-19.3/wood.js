d36bf19dc5a246bb9e647b4be54d82b2a0a1e9de
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-08 12:26:44 +0600

    added

---
+++ Milstone-3/Module-19/module-19.3/wood.js
@@ -0,0 +1,16 @@
+function woodQuantity(chairQuantity, tableQuantity, bedQuantity) {
+  const perChairwood = 5;
+  const perTablewood = 15;
+  const perBedwood = 30;
+
+  const chairTotalwood = chairQuantity * perChairwood;
+  const tableTotalwood = tableQuantity * perTablewood;
+  const bedTotalwood = bedQuantity * perBedwood;
+
+  const totalWood = chairTotalwood + tableTotalwood + bedTotalwood;
+
+  return totalWood;
+
+}
+const wood = woodQuantity(4, 2, 1);
+console.log(wood);
