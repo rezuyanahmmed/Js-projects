d19845117670f4c89c311f51a33adb0de03ef355
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-15 15:44:52 +0600

    commit

---
+++ Milstone-4/module-23  (Event)/module-23.3/style.js
@@ -0,0 +1,4 @@
+// document.getElementById().addEventListener('event type', handler)  {eta structure}
+document.getElementById('btn-make-green').addEventListener('click', function makeGreen(){
+  document.body.style.backgroundColor = 'lightgreen'
+})
