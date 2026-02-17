cc0103b48c7fc21ac6e2119edcc325f1356c9e5d
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-17 13:04:41 +0600

    commit

--- Milstone-4/module-23  (Event)/module-23.8/delegate.js
+++ Milstone-4/module-23  (Event)/module-23.8/delegate.js
@@ -1,9 +1,28 @@
-const items = document.getElementsByClassName('item');
-for(const item of items){
-  item.addEventListener('click', function(event){
-  event.target.parentNode.removeChild(event.target);
-    
-  })
+// const items = document.getElementsByClassName('item');
+// for(const item of items){
+//   item.addEventListener('click', function(event){
+//   event.target.parentNode.removeChild(event.target);
+//   })
+// }
+
+
+document.getElementById('item-list').addEventListener('click', function(event){
+  console.log(event.target);
   
-}
+})
+
+// Add e new item
+document.getElementById('btn-add-item').addEventListener('click', function(){
+  // parent to be added
+  const ol = document.getElementById('item-list')
+
+  //new item
+  const li = document.createElement('li');
+  li.classList.add('item');
+  li.innerText = 'Brand new item added'
+
+  // add the item
+  ol.appendChild(li);
+  
+})
 
