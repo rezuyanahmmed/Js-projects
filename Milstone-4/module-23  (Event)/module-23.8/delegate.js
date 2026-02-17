d3bfa590707772e31c0d4e1f066a4293e267e6d1
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-17 12:47:16 +0600

    commit

---
+++ Milstone-4/module-23  (Event)/module-23.8/delegate.js
@@ -0,0 +1,9 @@
+const items = document.getElementsByClassName('item');
+for(const item of items){
+  item.addEventListener('click', function(event){
+  event.target.parentNode.removeChild(event.target);
+    
+  })
+  
+}
+
