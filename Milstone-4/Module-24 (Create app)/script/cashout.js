b4bff75098ee34a8d32de2823a29aecb01feb313
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-18 19:36:24 +0600

    commit

--- Milstone-4/Module-24 (Create app)/script/cashout.js
+++ Milstone-4/Module-24 (Create app)/script/cashout.js
@@ -3,9 +3,13 @@ document.getElementById('cashout-btn').addEventListener('click', function () {
   const cashoutNumberInput = document.getElementById('cashout-number');
   const cashoutNumber = cashoutNumberInput.value;
   console.log(cashoutNumber);
+  if (cashoutNumber.length != 11){
+    alert('invalid agent number')
+    return;
+  }
 
-  //2- get the ammount
-  const cashoutAmmountInput = document.getElementById('cashout-ammount');
+    //2- get the ammount
+    const cashoutAmmountInput = document.getElementById('cashout-ammount');
   const cashoutAmmount = cashoutAmmountInput.value;
   console.log(cashoutAmmount);
 
@@ -18,10 +22,25 @@ document.getElementById('cashout-btn').addEventListener('click', function () {
   const newBalance = Number(balance) - Number(cashoutAmmount);
   if (newBalance < 0) {
     alert('invalid ammount')
+    return;
   }
-  console.log('new balance is:', newBalance);
+
 
   //5- get the pin and verify
-  //5.1 true:: show on alert > set balance
-  //5.2 false:: show an eror alert > return
+  const cashoutPinInput = document.getElementById('cashout-pin');
+  const pin = cashoutPinInput.value;
+  if (pin === '1234') {
+    //5.1 true:: show on alert > set balance
+    alert('cashout successfull');
+    console.log('new balance is:', newBalance);
+    balanceElement.innerText = newBalance;
+  }
+  else {
+    //5.2 false:: show an eror alert > return
+    alert('invalid pin')
+    return;
+  }
+
+
+
 })
