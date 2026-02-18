c1db33392441b6b3ec93698bd15235397d50efb2
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-18 19:14:06 +0600

    commit

--- Milstone-4/Module-24 (Create app)/script/cashout.js
+++ Milstone-4/Module-24 (Create app)/script/cashout.js
@@ -1,8 +1,26 @@
-document.getElementById('cashout-btn').addEventListener('click', function(){
+document.getElementById('cashout-btn').addEventListener('click', function () {
   //1- get the agent number and validet
-  //2- get the ammount, validet, convert to number
-  //3- get the balance, validet, convert to number
+  const cashoutNumberInput = document.getElementById('cashout-number');
+  const cashoutNumber = cashoutNumberInput.value;
+  console.log(cashoutNumber);
+
+  //2- get the ammount
+  const cashoutAmmountInput = document.getElementById('cashout-ammount');
+  const cashoutAmmount = cashoutAmmountInput.value;
+  console.log(cashoutAmmount);
+
+  //3- get the current balance
+  const balanceElement = document.getElementById('balance');
+  const balance = balanceElement.innerText;
+  console.log(balance);
+
   //4- calculate new balance
+  const newBalance = Number(balance) - Number(cashoutAmmount);
+  if (newBalance < 0) {
+    alert('invalid ammount')
+  }
+  console.log('new balance is:', newBalance);
+
   //5- get the pin and verify
   //5.1 true:: show on alert > set balance
   //5.2 false:: show an eror alert > return
