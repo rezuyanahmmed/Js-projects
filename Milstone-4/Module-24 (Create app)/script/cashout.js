666248e4e9786a15b72e907f6d0efe97c70c6fd4
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-18 17:56:17 +0600

    commit

---
+++ Milstone-4/Module-24 (Create app)/script/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById('cashout-btn').addEventListener('click', function(){
+  //1- get the agent number and validet
+  //2- get the ammount, validet, convert to number
+  //3- get the balance, validet, convert to number
+  //4- calculate new balance
+  //5- get the pin and verify
+  //5.1 true:: show on alert > set balance
+  //5.2 false:: show an eror alert > return
+})
