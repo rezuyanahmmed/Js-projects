2a98de93c016575a85643510a5c72fe2d39fc75e
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-18 12:50:41 +0600

    commit

--- Milstone-4/Module-24 (Create app)/script/login.js
+++ Milstone-4/Module-24 (Create app)/script/login.js
@@ -1,1 +1,24 @@
-console.log('okay');
+document.getElementById('login-btn').addEventListener('click', function () {
+  // 1-get the mobile number input
+  const numberInput = document.getElementById('input-number');
+  const contactNumber = numberInput.value;
+  console.log(contactNumber);
+
+  // 2-get the pin input
+  const inputPin = document.getElementById('input-pin');
+  const pin = inputPin.value;
+  console.log(pin);
+
+  // 3-match pin and mobile number
+  if (contactNumber == '01201234567' && pin == '1234') {
+    // 3.1- true:::>>alert> homepage
+    alert('login success')
+    window.location.assign('homepage.html')
+  }
+  else {
+    // 3.2- false:::>>alert> return
+    alert('login failed')
+    return;
+  }
+
+})
