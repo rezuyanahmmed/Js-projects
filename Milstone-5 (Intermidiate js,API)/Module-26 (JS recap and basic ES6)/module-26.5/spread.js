cc57d2f0263b95cb2ba36a407a5465f82229e418
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-27 01:44:44 +0600

    commit

---
+++ Milstone-5 (Intermidiate js,API)/Module-26 (JS recap and basic ES6)/module-26.5/spread.js
@@ -0,0 +1,4 @@
+// 26-5 Spread operator, array max, copy arrays
+const numbers = [5, 8, 4, 7, 6, 9, 1];
+const max = Math.max(...numbers); //ekhane 3 ta dot manei holo spread er moto kore dewa
+console.log(...numbers);
