73bd98b1e26e37332daa9a00f99a5fed3f8aa090
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-26 22:24:26 +0600

    first commit

--- Milstone-5 (Intermidiate js,API)/Module-26 (JS recap and basic ES6)/module-26.1/let,const.js
+++ Milstone-5 (Intermidiate js,API)/Module-26 (JS recap and basic ES6)/module-26.1/let,const.js
@@ -1,1 +1,9 @@
 // 26-1 ES6 intro: difference between var, let, and const
+
+// const diye kono variable declare korle ei same value te r kono value declare korte parbo na,,
+
+// let dorkar porbe value change korte hole..
+
+const name = 'rezuyan'
+console.log(name);
+
