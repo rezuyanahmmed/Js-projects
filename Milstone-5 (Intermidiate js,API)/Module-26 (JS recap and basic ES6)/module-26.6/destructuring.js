fbdb3398f93d39bb30884ee20a6cc6f4311038f4
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-27 23:32:26 +0600

    commit

---
+++ Milstone-5 (Intermidiate js,API)/Module-26 (JS recap and basic ES6)/module-26.6/destructuring.js
@@ -0,0 +1,14 @@
+// 26-6 (Advanced) Object and array destructuring
+const product = {
+  name: 'iphone',
+  price: 72000,
+  description: 'a smartphone by apple'
+};
+
+const newPrice = product.price + 100;
+console.log(newPrice);
+
+
+//---------------------
+const { price } = { name: 'Redmi', price: 25000, brand: 'xiomi' };
+console.log(price); //ekhane amra object er ekta faka variable nichi and ei faka variable e object er vitorer property er name diye oi faka object theke oi property er value ta output e pacchi...
