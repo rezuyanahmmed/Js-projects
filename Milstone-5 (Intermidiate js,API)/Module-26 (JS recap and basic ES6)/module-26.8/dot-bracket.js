fa5a9af89f1fcf8dea738b2b3672493785c3f205
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-28 00:32:17 +0600

    commit

---
+++ Milstone-5 (Intermidiate js,API)/Module-26 (JS recap and basic ES6)/module-26.8/dot-bracket.js
@@ -0,0 +1,17 @@
+//26-8 Accessing object data: nested object, dot vs bracket and optional chaining
+
+const product = {
+  name: 'rafi',
+  job: 'software developer',
+  1: 'home'
+  
+}
+
+// dot notation
+console.log(product.name);
+console.log(product.job);
+
+//bracket notation
+console.log(product[1]); //
+
+
