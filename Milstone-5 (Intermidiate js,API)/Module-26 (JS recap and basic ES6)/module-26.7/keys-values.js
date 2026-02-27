63440bb390b159f9ff0251ad0c776183946a73f3
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-27 23:57:28 +0600

    commit

--- Milstone-5 (Intermidiate js,API)/Module-26 (JS recap and basic ES6)/module-26.7/keys-values.js
+++ Milstone-5 (Intermidiate js,API)/Module-26 (JS recap and basic ES6)/module-26.7/keys-values.js
@@ -1,5 +1,10 @@
 // 26-7 Keys, values, entries, delete, seal, freeze
 const developer = { name: 'Rizuyan', age: 20, kingdom: 'Cumilla' }
-const keys = Object.keys(developer); // ekhane amra korchi object.keys er vitor main object er variable name ta dichi
+const keys = Object.keys(developer);
+ // ekhane amra korchi object.keys er vitor main object er variable name ta dichi...
+
+ const values = Object.values(developer);
+ console.log(values);
+ 
 console.log(keys); // property er name gula output e dekhar jonno...
 
