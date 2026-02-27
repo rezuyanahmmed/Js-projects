8527d2fd5168ca09394da4f13af439811cf7da10
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-02-28 01:07:02 +0600

    commit

---
+++ Milstone-5 (Intermidiate js,API)/Module-26 (JS recap and basic ES6)/module-26.9/loop.js
@@ -0,0 +1,17 @@
+//26-9 Looping objects: for...in, for...of
+const numbers = [5, 6, 1, 85, 1, 6, 7];
+for (const num of numbers) {
+  // console.log(num);
+
+}
+
+//-------------------
+const employe = {
+  name: 'jhon doe',
+  1: 'desk one',
+  salary: 40000,
+}
+for(const key in employe){
+  console.log(key);
+  
+}
