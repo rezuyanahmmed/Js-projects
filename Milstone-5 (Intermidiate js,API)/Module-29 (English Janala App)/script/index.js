9e7867d6548d71b6007b52d146ed5af9f2de0a35
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-03-05 23:41:08 +0600

    commit

--- Milstone-5 (Intermidiate js,API)/Module-29 (English Janala App)/script/index.js
+++ Milstone-5 (Intermidiate js,API)/Module-29 (English Janala App)/script/index.js
@@ -15,6 +15,20 @@ const displayLevelWord = (words) => {
   const wordContainer = document.getElementById('word-container');
   wordContainer.innerHTML = "";
 
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+    <div class="text-center col-span-full space-y-2">
+      <img class="mx-auto" src="./assets/alert-error.png" alt="">
+      <p class="font-bangla text-[15px] text-[#524f4cf3]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+      <h2 class="font-bangla text-[30px]">নেক্সট Lesson এ যান</h2>
+    </div>
+
+
+
+    `;
+    return;
+  }
+
 
   // {
   //   "id": 82,
@@ -32,9 +46,9 @@ const displayLevelWord = (words) => {
     card.innerHTML = `
 
      <div class="bg-white rounded-[10px] text-center shadow-sm py-4 px-4">
-      <h2 class="font-bold text-2xl">${word.word}</h2>
+      <h2 class="font-bold text-2xl">${word.word ? word.word : "Shobdo pawa jayni"}</h2>
       <p class="font-semibold text-[13px] pt-3">Meaning /Pronounciation</p>
-      <div class="text-[18px] font-mediumfont-bangla pt-5">"${word.meaning} / ${word.pronunciation}"</div>
+      <div class="text-[18px] font-mediumfont-bangla pt-5">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি "} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায়নি"}"</div>
       <div class="flex justify-between items-center">
         <button class="btn mt-10 bg-[#1a90ff17] hover:bg-[#1A91FF]"><i class="fa-solid fa-circle-info"></i></button>
         <button class="btn mt-10 bg-[#1a90ff17] hover:bg-[#1A91FF]"><i class="fa-solid fa-volume-high"></i></button>
