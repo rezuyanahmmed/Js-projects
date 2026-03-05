6a3a721031dabbda0a290edb2b558369fba3f04e
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-03-05 16:10:08 +0600

    commit

--- Milstone-5 (Intermidiate js,API)/Module-29 (English Janala App)/script/index.js
+++ Milstone-5 (Intermidiate js,API)/Module-29 (English Janala App)/script/index.js
@@ -4,6 +4,30 @@ const loadLeassons = () => {
     .then((json) => displayLeasson(json.data));
 };
 
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById('word-container');
+  // wordContainer.innerHTML = "";
+
+  words.forEach((word) => {
+    console.log(word);
+    const card = document.createElement('div');
+    card.innerHTML = `
+    
+    `
+
+  });
+
+};
+
+
+
 const displayLeasson = (leassons) => {
   //1: get the container and empty
   const levelContainer = document.getElementById('level-container');
@@ -12,11 +36,11 @@ const displayLeasson = (leassons) => {
   //2:get into every leassonn
   for (let leasson of leassons) {
     console.log(leasson);
-    
+
     //3:create element
     const btnDiv = document.createElement('div');
     btnDiv.innerHTML = `
-    <button class="btn btn-outline btn-primary">
+    <button onclick="loadLevelWord(${leasson.level_no})" class="btn btn-outline btn-primary">
     <i class="fa-solid fa-book-open"></i>Leasson - ${leasson.level_no}<button>
     `;
 
