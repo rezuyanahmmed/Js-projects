4719ee188ea55ea17302316130916df8b72aefcd
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-03-11 21:52:08 +0600

    commit

---
+++ Milstone-5 (Intermidiate js,API)/Module-30.5/module-30.5-3,4/card.js
@@ -0,0 +1,37 @@
+const handleProduct = () => {
+  const productEL = document.getElementById('product');
+  const quantityEl = document.getElementById('quantity');
+
+  const product = productEL.value;
+  const quantity = quantityEl.value;
+  console.log('product add', product, quantity);
+
+  displayProduct(product, quantity)
+  addProductToCrat(product, quantity)
+
+  productEL.value = '';
+  quantityEl.value = '';
+}
+
+const getCart = () => {
+  const cart = {};
+  return cart;
+}
+
+const addProductToCrat = (product, quantity) => {
+  const cart = getCart();
+  cart[product] = quantity;
+  console.log('cart', cart);
+  
+}
+
+const displayProduct = (product, quantity) => {
+  const li = document.createElement('li');
+  li.innerText = `${product} : ${quantity}`
+
+  //get the ul
+  const ul = document.getElementById('product-container');
+  ul.appendChild(li);
+}
+
+// set local storage
