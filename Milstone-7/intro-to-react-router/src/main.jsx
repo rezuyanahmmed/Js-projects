6988b7fd3087e4a5f38eab0e5aa662a5a13f8cfd
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-30 02:32:01 +0600

    first commit

--- Milstone-7/intro-to-react-router/src/main.jsx
+++ Milstone-7/intro-to-react-router/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
 
 import {
   createBrowserRouter,
@@ -13,6 +13,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <div>Hello World</div>,
   },
+  {
+    path: 'blogs',
+    element: <div>All my blogs here</div>
+  }
 ]);
 
 createRoot(document.getElementById('root')).render(
