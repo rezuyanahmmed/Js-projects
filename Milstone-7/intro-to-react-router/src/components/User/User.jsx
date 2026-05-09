3ba439f49a0a99ec0ca8bf841e787763dd031324
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-05-10 01:17:13 +0600

    first commit

--- Milstone-7/intro-to-react-router/src/components/User/User.jsx
+++ Milstone-7/intro-to-react-router/src/components/User/User.jsx
@@ -4,8 +4,15 @@ const User = ({ user }) => {
 
   const { name, email, phone } = user;
 
+  const userStyle = {
+    border: '2px solid yellow',
+    borderRadius: '20px',
+    padding: '10px',
+    margin: '10px'
+  }
+
   return (
-    <div>
+    <div style={userStyle}>
       <h2>Name:{name}</h2>
       <p>Email:{email} </p>
       <p><small>Phone Number: {phone}</small></p>
