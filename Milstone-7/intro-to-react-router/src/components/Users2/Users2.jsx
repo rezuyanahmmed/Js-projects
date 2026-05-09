37c9843abf635f11e4c97118a02f42509e075929
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-05-10 01:04:30 +0600

    first commit

--- Milstone-7/intro-to-react-router/src/components/Users2/Users2.jsx
+++ Milstone-7/intro-to-react-router/src/components/Users2/Users2.jsx
@@ -4,11 +4,11 @@ const Users2 = ({ userPromise }) => {
 
   const users = use(userPromise);
   console.log(users);
-  
+
 
   return (
     <div>
-
+      <h2>this is user 2</h2>
     </div>
   );
 };
